2cf4530472f3a64bd7d5e2f9fef1bc4f9c81a90d
Pabloescobarxde <Pabloescobar20peto@gmail.com>
2025-08-04 21:39:48 +0100

    Update rich_rpc.py

--- stelarys/src/presence/rich_rpc.py
+++ stelarys/src/presence/rich_rpc.py
@@ -1,22 +1,26 @@
 from stelarys.src.utiles.imports import *
+from pypresence import Presence
+import time
 
 def iniciar_rpc():
-
     CLIENT_ID = "1373326189586026547"
     START_TIME = int(time.time())
 
-    rpc = Presence(CLIENT_ID)
-    rpc.connect()
+    try:
+        rpc = Presence(CLIENT_ID)
+        rpc.connect()
 
-    rpc.update(
-        details="🔥 Mejor Herramienta de minecraft",
-        state="👿 Usando Stelarys",
-        start=START_TIME,
-        large_image="logotipo",
-        large_text="Stelarys - Potencia total",
-        small_image="r",
-        small_text="El Mejor",
-        buttons=[
-            {"label": "Unirme al Discord", "url": "https://discord.gg/viperfinder"}
-        ]
-    )
+        rpc.update(
+            details="🔥 Mejor Herramienta de minecraft",
+            state="👿 Usando Stelarys",
+            start=START_TIME,
+            large_image="logotipo",
+            large_text="Stelarys - Potencia total",
+            small_image="r",
+            small_text="El Mejor",
+            buttons=[
+                {"label": "Unirme al Discord", "url": "https://discord.gg/viperfinder"}
+            ]
+        )
+    except Exception as e:
+        pass
